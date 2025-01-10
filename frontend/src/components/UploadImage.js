823dd0969b76d3e9b5d9a0e28650a6ace16da36f
Raj Singh <152046820+Raj-Singh-3@users.noreply.github.com>
2025-01-10 09:36:53 +0530

    Update UploadImage.js

--- frontend/src/components/UploadImage.js
+++ frontend/src/components/UploadImage.js
@@ -23,7 +23,7 @@ const UploadImage = () => {
     formData.append('image', image);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/upload', {
+      const response = await fetch('https://image-backend-mk52.onrender.com', {
         method: 'POST',
         body: formData,
       });
